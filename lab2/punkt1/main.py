80c97305274bf1bec4070c875b6c62ee30445860
Pozivay Paul <nataliakrinkina2005@gmail.com>
2024-11-11 18:59:49 +0300

    laba2+lab1.3

--- lab2/punkt1/main.py
+++ lab2/punkt1/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 data = pd.read_csv('transactions.csv')
+#print(data)
 target = data.loc[data['STATUS'] == 'OK']
 target = target.sort_values(by='SUM', ascending=False)
 print("3 самых крупных платежа из реально проведённых (статус OK)")
