5909edd26f135ea9ce6ed37dd1a654e0e166e299
Pozivay Paul <nataliakrinkina2005@gmail.com>
2024-11-16 23:23:34 +0300

    error debug

--- lab1/punkt1/main.py
+++ lab1/punkt1/main.py
@@ -1,5 +1,13 @@
 import matplotlib.pyplot as plt
 
+xmin, xmax, ymin, ymax = 0, 0, 0, 0
+for i in range(1, 6):
+    name = "dead_moroz/00" + str(i) + ".dat"
+    num = int(open(name, "r").readline())
+    f = open(name).readlines()
+    xmin, xmax = min([float(i.split()[0]) for i in f[1:num]]), max([float(i.split()[0]) for i in f[1:num]])
+    ymin, ymax = min([float(i.split()[1]) for i in f[1:num]]), max([float(i.split()[1]) for i in f[1:num]])
+
 for i in range(1, 6):
     name = "dead_moroz/00" + str(i) + ".dat"
     f = open(name).readlines()
@@ -10,6 +18,8 @@ for i in range(1, 6):
         y.append(float(s[1]))
 
     fig, ax = plt.subplots()
+    ax.set_xlim([-50 + xmin, xmax + 50])
+    ax.set_ylim([-50 + ymin, ymax + 50])
     ax.scatter(x, y, marker='x', linestyle='-', color='red', linewidth=0.8)
     #plt.legend()
     # включаем основную сетку
--- lab1/punkt3/main.py
+++ lab1/punkt3/main.py
@@ -1,105 +1,80 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-df = pd.read_csv('students.csv')
-data = []
-for row in df.itertuples(index=True, name=None):
-    data.append(row[1].split(';'))
-for s in data:
-    s[2] = int(s[2])
-df = pd.DataFrame(data, columns=['prep', 'group', 'result'])
-#print(df)
-preps = dict()
-groups = dict()
-for i in df['prep']:
-    preps[i] = [0,0,0,0,0,0,0,0,0]
-for i in df['group']:
-    groups[i] = [0,0,0,0,0,0,0,0,0]
 
-for prep in preps:
-    target = df[df['prep'] == prep]
-    #print(target)
-    for row in target['result']:
-        #print(row)
-        preps[prep][row-3] += 1
-    mean = target['result'].sum() / len(target)
-    preps[prep][8] = mean.round(1)
-for group in groups:
-    target = df[df['group'] == group]
-    #print(target)
-    for row in target['result']:
-        #print(row)
-        groups[group][row-3] += 1
-    mean = target['result'].sum() / len(target)
-    groups[group][8] = mean.round(1)
+# Чтение данных
+df = pd.read_csv('students.csv', sep=';', names=['prep', 'group', 'result'])
+df['result'] = df['result'].astype(int)
 
+# Группировка данных
+preps = df.groupby('prep')['result'].value_counts().unstack(fill_value=0)
+preps['mean'] = df.groupby('prep')['result'].mean()
 
+groups = df.groupby('group')['result'].value_counts().unstack(fill_value=0)
+groups['mean'] = df.groupby('group')['result'].mean()
 
 grades = list(range(3, 11))
 
+
 fig, axs = plt.subplots(2, 1, figsize=(12, 6))
-prep_names = list(preps.keys())
+width = 0.08
+
+# Преподаватели
+prep_names = preps.index
 x_prep = np.arange(len(prep_names))
-width = 0.08  # Ширина столбцов
 
 for i, grade in enumerate(grades):
-    prep_counts = [preps[prep][i] for prep in prep_names]
-    axs[0].bar(x_prep + i * width, prep_counts, width, label=f'Result {grade}')
+    if grade in preps:
+        axs[0].bar(x_prep + i * width, preps[grade], width, label=f'Result {grade}')
 
-axs[0].set_xticks(x_prep + width * (len(grades) - 2) / 2)
+axs[0].set_xticks(x_prep + width * (len(grades) - 1) / 2)
 axs[0].set_xticklabels(prep_names)
 axs[0].set_xlabel('Preps')
 axs[0].set_ylabel('Number of results')
 axs[0].set_title('Results by prep')
-axs[0].legend(title='Result', loc='center right')
+axs[0].legend(title='Result', loc='center left', bbox_to_anchor=(1, 0.5))
 
-
-group_names = list(groups.keys())
+# Группы
+group_names = groups.index
 x_group = np.arange(len(group_names))
 
 for i, grade in enumerate(grades):
-    group_counts = [groups[group][i] for group in group_names]
-    axs[1].bar(x_group + i * width, group_counts, width, label=f'Result {grade}')
+    if grade in groups:
+        axs[1].bar(x_group + i * width, groups[grade], width, label=f'Result {grade}')
 
-axs[1].set_xticks(x_group + width * (len(grades) - 2) / 2)
+axs[1].set_xticks(x_group + width * (len(grades) - 1) / 2)
 axs[1].set_xticklabels(group_names)
 axs[1].set_xlabel('Group')
 axs[1].set_ylabel('Number of results')
-axs[1].set_title('Result by group')
-axs[1].legend(title='Result', loc='center right')
+axs[1].set_title('Results by group')
+axs[1].legend(title='Result', loc='center left', bbox_to_anchor=(1, 0.5))
 
 plt.tight_layout()
 
-
-
+# Построение средних результатов
 fig, axs = plt.subplots(2, 1, figsize=(12, 6))
-categories_prep = list(preps.keys())
-categories_group = list(groups.keys())
-mean_prep = [preps[prep][8] for prep in preps]
-mean_group = [groups[group][8] for group in groups]
-#print(mean_prep)
 
-colors_prep = [(165/255, 42/255, 42/255),
-               (0, 128/255, 0),
-               (0, 255/255, 0),
-               (139/255, 0, 0),
-               (205/255, 92/255, 0),
-               (255/255, 140/255, 0),
-               (173/255, 255/255, 47/255)]
-axs[0].bar(categories_prep, mean_prep, color=colors_prep)
+colors_prep = [(165 / 255, 42 / 255, 42 / 255),
+               (0, 128 / 255, 0),
+               (0, 255 / 255, 0),
+               (139 / 255, 0, 0),
+               (205 / 255, 92 / 255, 0),
+               (255 / 255, 140 / 255, 0),
+               (173 / 255, 255 / 255, 47 / 255)]
+axs[0].bar(preps.index, preps['mean'], color=colors_prep[:len(preps)])
 axs[0].set_title('Mean of preps')
 axs[0].set_xlabel('Prep')
 axs[0].set_ylabel('Average result')
 
 colors_group = [
-    (139/255, 0, 0),
-    (255/255, 99/255, 71/255),
-    (255/255, 165/255, 0),
-    (34/255, 139/255, 34/255),
-    (0, 128/255, 0)
+    (139 / 255, 0, 0),
+    (255 / 255, 99 / 255, 71 / 255),
+    (255 / 255, 165 / 255, 0),
+    (34 / 255, 139 / 255, 34 / 255),
+    (0, 128 / 255, 0)
 ]
-axs[1].bar(categories_group, mean_group, color=colors_group)
-axs[1].set_title('Mean of group')
+axs[1].bar(groups.index, groups['mean'], color=colors_group[:len(groups)])
+axs[1].set_title('Mean of groups')
 axs[1].set_xlabel('Group')
 axs[1].set_ylabel('Average result')
 
